2c7bc4eeb52823128b1254691e6a9d8ad9a4d958
Zoltan <matt.hill@dcind.net>
2018-11-29 15:05:37 -0800

    add custom label Clock with some built in display logic

---
+++ Timer.Test/ClockFixture.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Timer.Forms;
+using Xunit;
+
+namespace Timer.Test.ClockFixture
+{
+    public class given
+    {
+        private Clock _sut = new Clock();
+
+        public static IEnumerable<object[]> TestData()
+        {
+            yield return new object[] {-15,"-0:15" };
+            yield return new object[] {0,"0:00" };
+            yield return new object[] {15,"0:15" };
+            yield return new object[] {65,"1:05" };
+            yield return new object[] {605,"10:05" };
+        }
+
+        [Theory, MemberData(nameof(TestData))]
+        public void an_integer_count_of_seconds(int currentTime, string correctlyFormatted)
+        {
+            _sut.Display(currentTime);
+                
+            _sut.Text.Should().Be(correctlyFormatted);
+        }
+    }
+}
---
+++ Timer/Timer/Forms/Clock.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Timer.Forms
+{
+    public class Clock : Label
+    {
+        public void Display(int elapsedTime)
+        {
+            var time = TimeSpan.FromSeconds(elapsedTime);
+            var s = new StringBuilder();
+            if (elapsedTime < 0) s.Append('-');
+            s.Append(time.ToString(@"m\:ss"));
+            Text = s.ToString();
+        }
+    }
+}
